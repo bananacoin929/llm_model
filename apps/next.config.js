13528e1eedf3c1ae14e9e974952a1590c69f8b1b
bananacoin929 <bananacoin929@gmail.com>
2024-09-12 17:34:11 -0400

    fix: set image domain on next.config.js

--- apps/next.config.js
+++ apps/next.config.js
@@ -4,17 +4,7 @@ module.exports = {
   images: {
     domains: [
       'koppiesapi-app-20240813135717.calmtree-5c2b6c74.centralus.azurecontainerapps.io'
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "isomorphic-furyroad.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
+    ]
   },
   transpilePackages: ["@isomorphic/core"],
 };
