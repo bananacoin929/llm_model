8ea6f6a72b41dbced354db6b59ae3a8831484030
bananacoin929 <bananacoin929@gmail.com>
2024-09-12 20:08:21 -0400

    fix: 3

--- apps/src/app/page.tsx
+++ apps/src/app/page.tsx
@@ -283,26 +283,26 @@ export default function Home() {
     setIsGenerating(true)
     updateJson(data, tagJson, curLLmRequest.value).then(() => {
       runPromptRequest(data, tagJson, curLLmRequest.value).then((res) => {
-        console.log(res)
-        // const queryTotal = res.queryTotal,
-        //   displayResult = [];
-        // for (let i = 0; i < queryTotal; i++) {
-        //   const query = res[`queryNo${i + 1}`];
+        // console.log(res)
+        const queryTotal = res.queryTotal,
+          displayResult = [];
+        for (let i = 0; i < queryTotal; i++) {
+          const query = res[`queryNo${i + 1}`];
 
-        //   setIsGenerating(false);
-        //   if (
-        //     query.hasOwnProperty("response") &&
-        //     query.response.hasOwnProperty("url")
-        //   ) {
-        //     displayResult.push(query.response.url);
-        //   }
-        // }
+          setIsGenerating(false);
+          if (
+            query.hasOwnProperty("response") &&
+            query.response.hasOwnProperty("url")
+          ) {
+            displayResult.push(query.response.url);
+          }
+        }
 
-        // setOutputImgs(displayResult);
-        // setOutputJson(JSON.stringify(res, null, 2));
+        setOutputImgs(displayResult);
+        setOutputJson(JSON.stringify(res, null, 2));
       });
       setIsGenerating(false)
-    });
+    }).catch(err => console.log(err));
   };
 
   const onChangeTagpromptChildType = (index: number, value: string) => {
